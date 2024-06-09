34e32fe42e765d016b4ab90bf0bd6c158fc38d7a
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2024-06-09 11:57:01 +0700

    feat: lấy cái này cho dễ

--- src/main/java/com/doanSpringboot/repository/IBookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doanSpringboot.repository;
-
-import com.doanSpringboot.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBookRepository extends JpaRepository<Book, Long> {
-}
--- src/main/java/com/doanSpringboot/repository/ICategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.doanSpringboot.repository;
-
-import com.doanSpringboot.entity.Category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category, Long> {
-}
